deaf8227fa6d7473cf4cf3c68b3f722064da7826
Denis Voinovich <denzelvojn@gmail.com>
2023-05-04 08:02:48 +0300

    fix: add prettier and eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,20 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+  env: {
+    browser: true,
+        "es2021": true,
     },
-    "extends": "eslint:recommended",
+    "extends": {
+        "airbnb" : true,
+        "stylelint-config-prettier": true,
+    },
     "overrides": [
     ],
     "parserOptions": {
         "ecmaVersion": "latest",
         "sourceType": "module"
     },
-    "rules": {
+    rules: {
+        "semi": ["error", "never"],
+        "object-curly-newline": ["error", "always"],
     }
 }
